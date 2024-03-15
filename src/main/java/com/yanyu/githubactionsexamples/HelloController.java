86dca3eb57ae77d6e1f80a1d0f6341522a5f0556
烟雨平生9527 <1994124137@qq.com>
2024-03-16 07:46:50 +0800

    test6

--- src/main/java/com/yanyu/githubactionsexamples/HelloController.java
+++ src/main/java/com/yanyu/githubactionsexamples/HelloController.java
@@ -2,14 +2,17 @@ package com.yanyu.githubactionsexamples;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("hello")
     public String hello() {
         return Map.of("name", "yanyu1", "age", 18).toString();
     }
