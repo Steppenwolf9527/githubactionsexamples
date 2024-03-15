2b29641128c5189f45181902553c83a7819d04ee
烟雨平生9527 <1994124137@qq.com>
2024-03-15 22:54:48 +0800

    test5

--- src/main/java/com/yanyu/githubactionsexamples/HelloController.java
+++ src/main/java/com/yanyu/githubactionsexamples/HelloController.java
@@ -4,12 +4,14 @@ package com.yanyu.githubactionsexamples;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World55！！";
+        return Map.of("name", "yanyu", "age", 18).toString();
     }
 
 }
