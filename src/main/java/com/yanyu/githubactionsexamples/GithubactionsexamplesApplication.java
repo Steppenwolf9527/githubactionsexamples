4eb293234394791d7cbc80c1925f7281bfd54510
烟雨平生9527 <1994124137@qq.com>
2024-03-15 11:15:19 +0800

    init

---
+++ src/main/java/com/yanyu/githubactionsexamples/GithubactionsexamplesApplication.java
@@ -0,0 +1,13 @@
+package com.yanyu.githubactionsexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionsexamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubactionsexamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yanyu/githubactionsexamples/GithubactionsexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanyu.githubactionsexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionsexamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
